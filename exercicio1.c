a35a96e7288820a656d8e71cc518c33c259ebabc
NicolasFeitoza <nycolassf1@gmail.com>
2023-07-06 12:39:10 -0300

    codigo que imprime a frase pré-definida, inverte as letras da frase inteira, mas ainda nao inverte apenas as palavras

--- exercicio1.c
+++ exercicio1.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+void inverterPalavrasFrase(char frase[]) {
+	int tamanho=strlen(frase);
+	int letra_comeco=0;
+	int letra_final=tamanho-1;
+	while (letra_comeco<letra_final) {
+		char letra_atual=frase[letra_comeco];
+		frase[letra_comeco]=frase[letra_final];
+		frase[letra_final]=letra_atual;
+		letra_comeco++;
+		letra_final--;
+	}
+}
 int main(void) {
 	char frase[]="Hello, World! OpenAI is amazing.";
 	printf("Frase a ser invertida: %s\n", frase);
+	inverterPalavrasFrase(frase);
+	printf("Frase com as letras invertidas: %s\n", frase);
 	return 0;
 }
