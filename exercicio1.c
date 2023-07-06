6dcfb21c2c00d357d4128e6235f18118eb710b18
NicolasFeitoza <nycolassf1@gmail.com>
2023-07-06 13:29:24 -0300

    Mais algumas frases com o mesmo código

--- exercicio1.c
+++ exercicio1.c
@@ -27,8 +27,20 @@ void inverterPalavrasFrase(char frase[]) {
 	}
 }
 int main(void) {
-	char frase[]="Hello, World! OpenAI is amazing.";
-	inverterPalavrasFrase(frase);
-	printf("Frase invertida, mas com as palavras normais: %s\n", frase);
+	char frase1[]="Hello, how are you?";
+	char frase2[]="The quick brown fox jumps over the lazy dog.";
+	char frase3[]="I love programming in C.";
+	printf("Frase 1: %s\n", frase1);
+	inverterPalavrasFrase(frase1);
+	printf("Frase invertida, mas com as palavras normais: %s\n", frase1);
+	
+	printf("\nFrase 2: %s\n", frase2);
+	inverterPalavrasFrase(frase2);
+	printf("Frase invertida, mas com as palavras normais: %s\n", frase2);
+	
+	printf("\nFrase 3: %s\n", frase3);
+	inverterPalavrasFrase(frase3);
+	printf("Frase invertida, mas com as palavras normais: %s\n", frase3);
+	
 	return 0;
 }
