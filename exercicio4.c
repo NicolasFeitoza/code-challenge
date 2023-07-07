4cd74476e1fc4d71a07f07c8d61f9da87bbcc43a
NicolasFeitoza <nycolassf1@gmail.com>
2023-07-07 11:52:11 -0300

    Código 4 finalizado, entrega o que foi pedido

--- exercicio4.c
+++ exercicio4.c
@@ -14,21 +14,7 @@ void colocarEmMaiusculo(char string[]) {
 }
 int main(void) {
 	char string[]="hello. how are you? i'm fine, thank you.";
-	char string2[]="hello! how are you? i hope you're doing well.";
-	char string3[]="i love pizza. do you like it too?";
-	char string4[]="estamos indo para a praia! será que vai chover? espero que não.";
-	
-	printf("string que terá alguns caractéres colocados em maiúsculo: %s\n", string);
 	colocarEmMaiusculo(string);
-	printf("\nstring com letras de início de período em maiúsculo: %s\n", string);
-	printf("\nstring que terá alguns caractéres colocados em maiúsculo: %s\n", string2);
-	colocarEmMaiusculo(string2);
-	printf("\nstring com letras de início de período em maiúsculo: %s\n", string2);
-	printf("\nstring que terá alguns caractéres colocados em maiúsculo: %s\n", string3);
-	colocarEmMaiusculo(string3);
-	printf("\nstring com letras de início de período em maiúsculo: %s\n", string3);
-	printf("\nstring que terá alguns caractéres colocados em maiúsculo: %s\n", string4);
-	colocarEmMaiusculo(string4);
-	printf("\nstring com letras de início de período em maiúsculo: %s\n", string4);
+	printf("%s\n", string);
 	return 0;
 }
