3d9863e8a632763b508e560402ebbd53a16d8c64
NicolasFeitoza <nycolassf1@gmail.com>
2023-07-07 01:12:13 -0300

    Correção do código anterior(não funcionou com outros exemplos)

--- exercicio3.c
+++ exercicio3.c
@@ -1,31 +1,51 @@
 #include <stdio.h>
 #include <string.h>
+
 void encontrarPalindromo(char string[]) {
-	int tamanho=strlen(string);
-	int maiorSubString=0;
-	int inicio_maiorSubString=0;
-	int comeco, fim, tamanhoSubStr;
-	for (int i=0; i<tamanho; i++) {
-		for (int j=i+1; j<tamanho; j++) {
-			if (string[i]==string[j]) {
-				comeco=i;
-				fim=j;
-				tamanhoSubStr=j-i+1;
-				if (tamanhoSubStr>maiorSubString) {
-					maiorSubString=tamanhoSubStr;
-					inicio_maiorSubString=comeco;
-				}
-			}
-		}
-	}
-	printf("Substring palíndroma mais longa: ");
-	for (int i=inicio_maiorSubString; i<inicio_maiorSubString+maiorSubString; i++) {
-		printf("%c", string[i]);
-	}
+  int tamanho = strlen(string);
+  int maiorSubString = 0;
+  int inicio_maiorSubString = 0;
+  int segundoMaiorSubString = 0;
+  int inicio_segundoMaiorSubString = 0;
+  for (int i = 0; i < tamanho; i++) {
+    for (int j = i + 1; j < tamanho; j++) {
+      if (string[i] == string[j]) {
+        int comeco = i;
+        int fim = j;
+        int tamanhoSubStr = j - i + 1;
+        if (tamanhoSubStr > maiorSubString) {
+          segundoMaiorSubString = maiorSubString;
+          inicio_segundoMaiorSubString = inicio_maiorSubString;
+          maiorSubString = tamanhoSubStr;
+          inicio_maiorSubString = comeco;
+        } else if (tamanhoSubStr > segundoMaiorSubString && tamanhoSubStr < maiorSubString) {
+          segundoMaiorSubString = tamanhoSubStr;
+          inicio_segundoMaiorSubString = comeco;
+        }
+      }
+    }
+  }
+  if (maiorSubString == tamanho) {
+    if (segundoMaiorSubString != 0) {
+      printf("substring: ");
+      for (int i = inicio_segundoMaiorSubString; i < inicio_segundoMaiorSubString + segundoMaiorSubString; i++) {
+        printf("%c", string[i]);
+      }
+      printf("\n");
+    } else {
+      printf("Nao e um palindromo");
+    }
+  } else {
+        printf("Maior substring: ");
+        for (int i = inicio_maiorSubString; i < inicio_maiorSubString + maiorSubString; i++) {
+            printf("%c", string[i]);
+        }
+        printf("\n");
+  }
 }
 int main(void) {
-	char string[]="babad";
-	printf("String que será verificado se há uma substring palíndroma: %s\n", string);
-	encontrarPalindromo(string);
-	return 0;
+    char string[] = "babad";
+    printf("String que será verificada se há uma substring palindrômica: %s\n", string);
+    encontrarPalindromo(string);
+    return 0;
 }
