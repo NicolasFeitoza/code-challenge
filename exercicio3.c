c85416bd755a28b06d267de10ed3a5ae55edc55f
NicolasFeitoza <nycolassf1@gmail.com>
2023-07-07 01:37:18 -0300

    Código do exercício 3 finalizado, entregando o proposto

--- exercicio3.c
+++ exercicio3.c
@@ -27,16 +27,14 @@ void encontrarPalindromo(char string[]) {
   }
   if (maiorSubString==tamanho) {
     if (segundoMaiorSubString!=0) {
-      printf("substring: ");
       for (int i=inicio_segundoMaiorSubString; i<inicio_segundoMaiorSubString+segundoMaiorSubString; i++) {
         printf("%c", string[i]);
       }
       printf("\n");
     } else {
-      printf("Nao e um palindromo");
+      printf("Nao há substring palindroma");
     }
   } else {
-        printf("Maior substring: ");
         for (int i=inicio_maiorSubString; i<inicio_maiorSubString+maiorSubString; i++) {
             printf("%c", string[i]);
         }
@@ -45,24 +43,6 @@ void encontrarPalindromo(char string[]) {
 }
 int main(void) {
     char string1[]="babad";
-	char string2[]="aibofobia";
-	char string3[]="omississimo";
-	char string4[]="sopapos";
-	
-    printf("String que será verificada se há uma substring palindrômica: %s\n", string1);
     encontrarPalindromo(string1);
-   
-	printf("string 1 que será verificada se há uma substring palindrômica: %s\n", string2);
-	encontrarPalindromo(string2);
-	
-	
-	printf("\nstring 2 que será verificada se há uma substring palindrômica: %s\n", string3);
-	encontrarPalindromo(string3);
-	
-	
-	printf("\nstring 3 que será verificada se há uma substring palindrômica: %s\n", string4);
-	encontrarPalindromo(string4);
-	
-	
 	return 0;
 }
