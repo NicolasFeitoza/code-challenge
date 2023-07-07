98954d9ffaa100f301d9fe9a8afbfdcaccb7245a
NicolasFeitoza <nycolassf1@gmail.com>
2023-07-07 11:42:51 -0300

    Adição da função que coloca letras de início de período em maiusculo

--- exercicio4.c
+++ exercicio4.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+void colocarEmMaiusculo(char string[]) {
+	int tamanho=strlen(string);
+	string[0]=toupper(string[0]);
+	for (int i=1; i<tamanho; i++) {
+		if(string[i+1]=='\0') {
+			break;	
+		} else if (string[i]=='.' || string[i]=='?' || string[i]=='!') {
+				string[i+2]=toupper(string[i+2]);
+		}
+	}
+}
 int main(void) {
-	char string[]="hello. how are you? i'm fine, thank you";
-	printf("string que terá alguns caractéres colocados em maiúsculo: %s", string);
+	char string[]="hello. how are you? i'm fine, thank you.";
+	printf("string que terá alguns caractéres colocados em maiúsculo: %s\n", string);
+	colocarEmMaiusculo(string);
+	printf("\nstring com letras de início de período em maiúsculo: %s\n", string);
 }
