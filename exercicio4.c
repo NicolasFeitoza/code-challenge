ff198e34aac233073b7caf61d8054d62ad067377
NicolasFeitoza <nycolassf1@gmail.com>
2023-07-07 11:05:42 -0300

    Código base que apenas imprime a string que será modificada

---
+++ exercicio4.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+int main(void) {
+	char string[]="hello. how are you? i'm fine, thank you";
+	printf("string que terá alguns caractéres colocados em maiúsculo: %s", string);
+}
