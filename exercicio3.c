f17c27a08ece9901061f592a4d04e6329e5eeb7b
NicolasFeitoza <nycolassf1@gmail.com>
2023-07-06 23:44:06 -0300

    estrutura básica de impressão do exercício 3

---
+++ exercicio3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+int main(void) {
+	char string[]="babad";
+	printf("String que será verificado se há uma substring palíndroma: %s\n", string);
+	return 0;
+}
